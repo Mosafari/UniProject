1bf416d0af45328bc235e49d811c4a3608ca490f
mosafari <m.safari2680@gmail.com>
2023-05-15 09:45:13 +0430

    add more option

--- Books.cs
+++ Books.cs
@@ -21,7 +21,7 @@ namespace test1
             List<Books> books = new List<Books>();
             
             Console.WriteLine("Help :");
-            Console.WriteLine("enter 1 to add a book .\nenter 2 to del a book .\nenter 3 to update a book . \nenter 4 to show list .\nenter 5 to see this  help .\nand 0 to exit .\n"); 
+            Console.WriteLine("enter 1 to add a book .\nenter 2 to del a book .\nenter 3 to update a book . \nenter 4 to show list .\nenter 11 to see this  help .\nand 0 to exit .\n"); 
             while(true)
             {
                 Console.WriteLine("choose your action :");
@@ -65,7 +65,51 @@ namespace test1
                         Console.WriteLine("\n");
 
                 }
-                if(action == "5") // show help
+                if (action == "5") // sort books price and outputs
+                {
+                    List<Books> sortedList = books.OrderByDescending(x => x.price).ToList();
+                    for (int i = 0; i < sortedList.Count; i++)
+                    {
+                        Console.WriteLine(i + ", " + sortedList[i].bname + ", " + sortedList[i].author + ", " + sortedList[i].price);
+                    }
+
+                }
+                if (action == "6") // search with book name and outputs
+                    {
+                        Console.WriteLine("Enter book name to search:");
+                        string searchName = Console.ReadLine();
+
+                        List<Books> matchedBooks = books.Where(x => x.bname == searchName).ToList();
+                        if (matchedBooks.Count > 0)
+                        {
+                            Console.WriteLine("id, bookname, author, price ");
+                            for (int i = 0; i < matchedBooks.Count; i++)
+                            {
+                                Console.WriteLine(i + ", " + matchedBooks[i].bname + ", " + matchedBooks[i].author + ", " + matchedBooks[i].price);
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("No result.");
+                        }
+                    }
+                if (action == "7") // show book with the highest price
+                    {
+                        if (books.Count > 0)
+                        {
+                            Books maxPriceBook = books.OrderByDescending(x => x.price).FirstOrDefault();
+                            Console.WriteLine("Book with largest price: ");
+                            Console.WriteLine("bookname: ",maxPriceBook.bname);
+                            Console.WriteLine("author: ",maxPriceBook.author);
+                            Console.WriteLine("price: ",maxPriceBook.price);
+                        }
+                        else
+                        {
+                            Console.WriteLine("No result.");
+                        }
+                    }
+                if (action == "8") // show book with with the lowest price
+                if(action == "11") // show help
                 {
                     // Console.WriteLine("enter 1 to add a book .\nenter 1 to add a book .\nenter 2 to add a book .\nenter 3 to update a book . \
                     // \nenter 4 to show list .\nenter 5 to see this  help .\nand 0 to exit ."); 
@@ -76,7 +120,7 @@ namespace test1
                     Console.WriteLine("enter 2 to del a book .");
                     Console.WriteLine("enter 3 to update a book .");
                     Console.WriteLine("enter 4 to show list .");
-                    Console.WriteLine("enter 5 to see this  help .");
+                    Console.WriteLine("enter 11 to see this  help .");
                     Console.WriteLine("and 0 to exit .\n");
                 }
                 if(action == "0") // exit
